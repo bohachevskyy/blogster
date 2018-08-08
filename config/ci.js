82d02ca3dd25b054aba9f542ddc2f44ebdc79c0b
Dmytro Bohachevskyy <dmytro.bohachevskyy@gmail.com>
2018-08-09 00:48:49 +0300

    Travis config has been added

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://dmytro:MTz-2q3-kfh-QSU@ds259111.mlab.com:59111/blog_dev',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
